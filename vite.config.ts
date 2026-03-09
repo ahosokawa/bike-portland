e370ee75e4488501b9a93c792bf911297a541cdf
Andrew Hosokawa <hosokawa13+2@gmail.com>
2026-03-08 17:27:58 -0700

    update icon

--- vite.config.ts
+++ vite.config.ts
@@ -21,8 +21,11 @@ export default defineConfig({
         display: 'standalone',
         start_url: '/bike-portland/',
         icons: [
+          { src: 'favicon.ico', sizes: '16x16 32x32', type: 'image/x-icon' },
           { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: 'icons/icon-192-maskable.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
+          { src: 'icons/icon-512-maskable.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
         ],
       },
       workbox: {
